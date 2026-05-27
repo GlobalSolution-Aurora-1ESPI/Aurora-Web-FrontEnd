877e5b6ced1f8ded23c6f67c66d342cdcc50968b
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-05-27 19:08:21 -0300

    feat: Adicionando cores aos temas

--- Aurora-Web-Front/src/js/script.js
+++ Aurora-Web-Front/src/js/script.js
@@ -6,4 +6,22 @@ iniciarQuiz();
 function iniciarTemas() {
     const pagina = document.querySelector(".page-wrapper");
     const botoesTema = document.querySelectorAll('input[name="tema"]');
+
+    const temas = {
+        "tema-dia": {
+            bg: "#080D1A",
+            navy: "#0D1B2E",
+            amber: "#FF7A33",
+        },
+        "tema-noite": {
+            bg: "#000510",
+            navy: "#000B18",
+            amber: "#00C9FF",
+        },
+        "tema-alerta": {
+            bg: "#1A0000",
+            navy: "#0D0000",
+            amber: "#FF4444",
+        },
+    };
 }
