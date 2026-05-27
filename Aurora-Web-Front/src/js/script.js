9129c1c3529958f08a8c36763eff839af3676530
Eduardo Craveiro <eduardo.craveiro26@gmail.com>
2026-05-27 19:00:21 -0300

    feat: adiciona a página e o input

--- Aurora-Web-Front/src/js/script.js
+++ Aurora-Web-Front/src/js/script.js
@@ -3,3 +3,7 @@ iniciarTemas();
 iniciarSlideshow();
 iniciarFormulario();
 iniciarQuiz();
+function iniciarTemas() {
+    const pagina = document.querySelector(".page-wrapper");
+    const botoesTema = document.querySelectorAll('input[name="tema"]');
+}
